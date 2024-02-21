5ca9855ec768fea27bb1f58c5f77087f73357dff
Chris Cascioli <chriscascioli@gmail.com>
2024-02-21 12:04:07 -0500

    Singleton demo

---
+++ SingletonDemo/ExampleSingleton.cs
@@ -0,0 +1,71 @@
+﻿// Chris Cascioli
+// 2/21/24
+// Example of the Singleton design pattern
+
+namespace SingletonDemo
+{
+	internal class ExampleSingleton
+	{
+		// The problem we're trying to solve:
+		//  - Globally accessible object
+		//  - Only one can EVER be instantiated
+
+		/// <summary>
+		/// The one and only instance of this class
+		/// </summary>
+		private static ExampleSingleton instance = null!;
+
+		/// <summary>
+		/// Gets the one and only instance
+		/// </summary>
+		public static ExampleSingleton Instance
+		{
+			get
+			{
+				// If it doesn't exist, make it!
+				if (instance == null)
+					instance = new ExampleSingleton();
+
+				// Otherwise, just give back the one and only
+				return instance;
+			}
+		}
+
+		// Initialization logic
+		private bool initialized = false;
+
+		// Standard fields
+		private int numberOfThings;
+		private string name;
+
+		public int NumberOfThings { get { return numberOfThings; } }
+
+		public string Name { get { return name; } }
+
+		/// <summary>
+		/// Creates a barebones object.  The point
+		/// is that the constructor is PRIVATE
+		/// </summary>
+		private ExampleSingleton()
+		{
+
+		}
+
+		/// <summary>
+		/// The public Initialize function is essentially how we
+		/// get the object "ready", kind of like a constructor
+		/// </summary>
+		/// <param name="name">Name of thing</param>
+		/// <param name="numThings">Number of them</param>
+		public void Initialize(string name, int numThings)
+		{
+			if (initialized)
+				throw new InvalidOperationException("Singleton already initialized");
+
+			this.name = name;
+			this.numberOfThings = numThings;
+			initialized = true;
+		}
+
+	}
+}
---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SingletonDemo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Can we make a new singleton object?  NO!  On purpose!
+			//ExampleSingleton single = new ExampleSingleton();
+
+			// Grab the STATIC reference to get the actual object
+			ExampleSingleton.Instance.Initialize("Bobby", 6);
+
+			ExampleSingleton theOne = ExampleSingleton.Instance;
+
+			// Get any of the data
+			Console.WriteLine("Singleton name: " + theOne.Name);
+			Console.WriteLine("Singleton num: " + theOne.NumberOfThings);
+
+
+		}
+	}
+}
